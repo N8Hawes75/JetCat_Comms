a7d4ec696a82fd4854d765a78d290e8232fde692
ColtonWright51 <cw717317@ohio.edu>
2022-11-11 11:15:19 -0500

    Code saved from OneDrive

---
+++ crc.h
@@ -0,0 +1,4 @@
+float pi_approx(int n);
+u_int16_t crc16_update ( u_int16_t crc, u_int8_t data );
+u_int16_t get_crc16z(u_int8_t *p, u_int16_t len);
+
---
+++ read_port.py
@@ -0,0 +1,7 @@
+import serial
+import numpy as np
+import serial
+import time
+import pandas as pd
+import csv
+from help_cw import *
