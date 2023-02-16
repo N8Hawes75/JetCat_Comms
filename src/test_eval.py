0d3c00ccf26689b9f79935ac16a791706ec2e0c4
ColtonWright51 <cw717317@ohio.edu>
2023-02-16 16:46:14 -0500

    Adding more features

--- src/test_eval.py
+++ src/test_eval.py
@@ -13,7 +13,25 @@ import pandas as pd
 # print(type(cmd_array[3,1]))
 # return cmd_array
 
+def modify_value(value):
+    # This function will be applied to each value in the csv file. So check if
+    # a % is there, and change it to a RPM command instead of % command.
+    # also evaluate expressions so that 25+60 is just 85
 
+
+    if isinstance(value, str):
+        print("Value is a string: ", value)
+
+        if "%" in value:
+            value = float(value.strip('%'))
+            value = 700*value + 34000 # Map 0% to 34000rpm, 100% to 104000rpm
+        
+        if "+" in value:
+            value = eval(value)
+
+
+    print("Returning: ", value)
+    return value
 
 a1 = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
 print("a1:\n", a1)
@@ -33,7 +51,7 @@ print("f2:\n", f2)
 f3 = pd.read_csv("/home/colton/Documents/GitHub/JetCat_Comms/throttle_curves/throttle_curves_rpm/2023-02-16.txt")
 print("f3:\n", f3)
 
-f4 = f3.applymap(lambda x: eval(x) if isinstance(x, str) else x    )
+f4 = f3.applymap(modify_value)
 print("f4:\n", f4)
 
 a4 = f4.to_numpy()
